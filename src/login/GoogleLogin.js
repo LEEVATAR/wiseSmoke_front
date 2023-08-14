07714cf20dc681c8019126c40041b7794d931bf9
sososo95 <ssojae95@gmail.com>
2023-08-15 07:42:36 +0900

    Google, Naver ADD(서버 연동 해봐야함)

--- src/login/GoogleLogin.js
+++ src/login/GoogleLogin.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView } from 'react-native-webview';
+
+const GOOGLE_CLIENT_ID = '1009493733246-1kc1to0l3v088lele9ckk70d6jkssdvh.apps.googleusercontent.com'; // 구글 클라이언트 아이디
+const REDIRECT_URI = 'http://localhost:19006/'; // 리디렉션 URL
+const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const GoogleLogin = () => {
-  return (
-    <View style={styles.container}>
-      <Text>This is a new page!</Text>
-    </View>
-  );
-};
 
+  function GoogleLoginWebView (data) {
+    // 구글 로그인 처리 코드 작성
+  }
+
+  return (
+    <View style={Styles.container}>      
+      <WebView
+        style={{ flex: 1 }}
+        originWhitelist={['*']}
+        scalesPageToFit={false}
+        source={{
+          uri: `https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=wiseSmoke&scope=openid`,
+        }}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        javaScriptEnabled
+        onMessage={event => { GoogleLoginWebView(event.nativeEvent["url"]); }}
+      />
+    </View>
+  )
+}
 
 export default GoogleLogin;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 24,
+    backgroundColor: '#fff',
+  },    
+});
--- src/login/NaverLogin.js
+++ src/login/NaverLogin.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { WebView } from 'react-native-webview';
+
+const NV_APP_ID = 'i90dwXdfWfTQaPy11Qqq';
+const NV_SERCET_ID = 'dxyna27e4T';
+const REDIRECT_URI = 'http://localhost:19006/';
+const STATE_STRING = "wiseSmoke";
+const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const NaverLogin = () => {
-  return (
-    <View style={styles.container}>
-      <Text>This is a new page!</Text>
-    </View>
-  );
-};
 
+  // function NaverLoginWebView (data) {
+  //   const exp = "code=";
+  //   var condition = data.indexOf(exp);    
+  //   if (condition != -1) {
+  //     var authorize_code = data.substring(condition + exp.length);
+  //     console.log(authorize_code);
+  //   };
+  // }
+
+  return (
+    <View style={Styles.container}>      
+      <WebView
+        style={{ flex: 1 }}
+        originWhitelist={['*']}
+        scalesPageToFit={false}
+        source={{
+          uri: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NV_APP_ID}&redirect_uri=${encodeURIComponent(
+            REDIRECT_URI
+          )}&state=${STATE_STRING}`,
+        }}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        javaScriptEnabled
+      />
+    </View>
+  )
+}
 
 export default NaverLogin;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 24,
+    backgroundColor: '#fff',
+  },    
+});
