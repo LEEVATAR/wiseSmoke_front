b2bffcdab92e8b33db6fc48fe9a83624b6789479
sososo95 <ssojae95@gmail.com>
2023-08-22 02:15:53 +0900

    [JAEYEON] Google web Login 연동 성공(android 추가 작업 중)

--- src/login/GoogleLogin.js
+++ src/login/GoogleLogin.js
@@ -1,30 +1,66 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { WebView } from 'react-native-webview';
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 
-const GOOGLE_CLIENT_ID = '1009493733246-1kc1to0l3v088lele9ckk70d6jkssdvh.apps.googleusercontent.com'; // 구글 클라이언트 아이디
-const REDIRECT_URI = 'http://localhost:19006/'; // 리디렉션 URL
-const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
+// 495386511482-55tgacguhoag5a1aktni980q8knqco41.apps.googleusercontent.com // 구글 web 아이디
+// 495386511482-ggcgq3mc214kgc60prrvdphf9jsunsfs.apps.googleusercontent.com // ios 아이디
+// 495386511482-homka73t59afr96ckeh7c6uiccfjbahi.apps.googleusercontent.com // android 아이디
+
+WebBrowser.maybeCompleteAuthSession();
 
 const GoogleLogin = () => {
 
-  function GoogleLoginWebView (data) {
-    // 구글 로그인 처리 코드 작성
+  const [accessToken, setAccessToken] = React.useState(null);
+  const [user, setUser] = React.useState(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: "495386511482-55tgacguhoag5a1aktni980q8knqco41.apps.googleusercontent.com",
+    iosClientId: "495386511482-ggcgq3mc214kgc60prrvdphf9jsunsfs.apps.googleusercontent.com",
+    androidClientId: "495386511482-homka73t59afr96ckeh7c6uiccfjbahi.apps.googleusercontent.com"
+  });
+
+  React.useEffect(() => {
+    if(response?.type === "success"){
+      setAccessToken(response.authentication.accessToken);
+      accessToken && fetchUserInfo();
+    }
+  }, [response, accessToken])
+
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    const userInfo = await response.json();
+    setUser(userInfo);
+  }
+
+  const ShowUserInfo = () => {
+    if(user) {
+      return(
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image source={{uri: user.picture}} style={{width: 100, height: 100, borderRadius: 50}}></Image> 
+          <Text style={{fontSize: 20, fontWeight: 'bold', }}>{user.name}</Text>
+        </View>
+      )
+    }
   }
 
   return (
-    <View style={Styles.container}>      
-      <WebView
-        style={{ flex: 1 }}
-        originWhitelist={['*']}
-        scalesPageToFit={false}
-        source={{
-          uri: `https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=wiseSmoke&scope=openid`,
-        }}
-        injectedJavaScript={INJECTED_JAVASCRIPT}
-        javaScriptEnabled
-        onMessage={event => { GoogleLoginWebView(event.nativeEvent["url"]); }}
-      />
+    <View style={Styles.container}>
+      {user && <ShowUserInfo />}
+      {user === null && 
+        <>
+        <TouchableOpacity
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+          style={Styles.button}
+        >
+          <Text style={Styles.buttonText}>구글 로그인</Text>
+        </TouchableOpacity>
+        </>
+      }  
     </View>
   )
 }
@@ -34,7 +70,8 @@ export default GoogleLogin;
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 24,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },    
 });
